f1ae900c544ccb0f3fa406f7a0881e0807d29970
mayo <mayo.yamasaki@gmail.com>
2013-12-22 14:01:37 +0900

    fixed test

--- src/test/scala/LogisticRegressionSpec.scala
+++ src/test/scala/LogisticRegressionSpec.scala
@@ -6,50 +6,39 @@ class LogisticRegressionSpec extends FlatSpec {
 
     behavior of "A LogisticRegression"
 
-    it should "recursive trial" in {
-        val logisticregression = new LogisticRegression
-        val trial = PrivateMethod[List[Double]]('trial)
-
-        val gold = List((1.0, 4.0, 1.0),
-                        (-2.0, -1.0, -1.0))
-        val w:List[Double] = List[Double](0.0, 0.0, 0.0)
-        val real = logisticregression.train(gold, w, 2)
-        val ones_w = logisticregression invokePrivate trial(gold, w)
-        val expect = logisticregression invokePrivate trial(gold, ones_w)
-        assert(real === expect)
-    }
-
-
     it should "update w(a, b, c) parmeter in trial." in {
         val logisticregression = new LogisticRegression
         val trial = PrivateMethod[List[Double]]('trial)
 
         val gold = List((1.0, 2.0, 1.0))
         val w = List[Double](3.0, -4.0, 1.0)
-        val expect = List[Double](4.0, -2.0, 2.0)
-        val real:List[Double] = logisticregression invokePrivate trial(gold, w)
+        val expect = List[Double](3.098201379003791, -3.8035972419924184, 1.0982013790037908)
+        val real:List[Double] = logisticregression invokePrivate trial(gold, w, 0.1)
         assert(real !== w)
         assert(real === expect)
     }
 
-
-    it should "predicate if >=0 then true else false from two List." in {
+    it should "predicate return x | 0 <= x <= 1." in {
         val logisticregression = new LogisticRegression
         val predicate = PrivateMethod[Double]('predicate)
+        val sigmoid = PrivateMethod[Double]('sigmoid)
 
         val pos_one = List[Double](1.0)
         val zero = List[Double](0.0)
         val neg_one = List[Double](-1.0)
 
-        val pos_pos = logisticregression invokePrivate predicate(pos_one, pos_one)
-        val pos_zero = logisticregression invokePrivate predicate(pos_one, zero)
-        val pos_neg = logisticregression invokePrivate predicate(pos_one, neg_one)
+        val real1 = logisticregression invokePrivate predicate(pos_one, pos_one)
+        val real2 = logisticregression invokePrivate predicate(pos_one, zero)
+        val real3 = logisticregression invokePrivate predicate(pos_one, neg_one)
 
-        assert(pos_pos === 1.0)
-        assert(pos_zero === 1.0)
-        assert(pos_neg === -1.0)
+        val expect1 = logisticregression invokePrivate sigmoid(1.0)
+        val expect2 = logisticregression invokePrivate sigmoid(0.0)
+        val expect3 = logisticregression invokePrivate sigmoid(-1.0)
+
+        assert(real1 === expect1)
+        assert(real2 === expect2)
+        assert(real3 === expect3)
     }
-
 
     it should "inner product from two List." in {
         val logisticregression = new LogisticRegression
@@ -69,6 +58,14 @@ class LogisticRegressionSpec extends FlatSpec {
 
         val real:List[Double] = logisticregression invokePrivate phi(1.0,2.0)
         val expect = List[Double](1.0, 2.0, 1.0)
+        assert(real === expect)
+    }
+
+    it should "sigmoid fuction" in {
+        val logisticregression = new LogisticRegression
+        val sigmoid = PrivateMethod[Double]('sigmoid)
+        val real:Double = logisticregression invokePrivate sigmoid(0.0)
+        val expect:Double = 0.5
         assert(real === expect)
     }
 
